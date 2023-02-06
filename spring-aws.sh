61b62cdbd6d221cc8907ac9fc99a71d8d6158105
JaimeUin <jaimelagos@MacBook-Pro-de-Jaime.local>
2023-02-06 12:50:53 -0300

    endpoint

--- spring-aws.sh
+++ spring-aws.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar spring-aws/target/spring-aws-0.0.1-SNAPSHOT.jar server config.json
+java -jar ./home/ubuntu/actions-runner/_work/spring-aws/spring-aws/target/spring-aws-0.0.1-SNAPSHOT.jar config.json
--- src/main/java/com/example/springaws/UserController.java
+++ src/main/java/com/example/springaws/UserController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @GetMapping("info")
+    public ResponseEntity<ServiceInformation> getInfo() {
+        return ResponseEntity.ok(infoService.get());
+    }
     @GetMapping
     public String demo(){
         return "buenas buenas!";
