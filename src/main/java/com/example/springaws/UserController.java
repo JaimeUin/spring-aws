157ea0884f5f763d38210ef75933ab95ef6123bb
JaimeUin <jaimelagos@MacBook-Pro-de-Jaime.local>
2023-02-06 13:10:40 -0300

    endpoint 1

--- src/main/java/com/example/springaws/UserController.java
+++ src/main/java/com/example/springaws/UserController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("info")
-    public ResponseEntity<ServiceInformation> getInfo() {
-        return ResponseEntity.ok(infoService.get());
-    }
-    @GetMapping
     public String demo(){
         return "buenas buenas!";
     }
