23d3c46989f0f760a2beced5314b3aee6b21dd93
JaimeUin <jaimelagos@MacBook-Pro-de-Jaime.local>
2023-02-06 11:43:08 -0300

    java sh 1

--- spring-aws.sh
+++ spring-aws.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-	java -jar spring-aws/target/spring-aws-0.0.1-SNAPSHOT.jar
-#  run: sudo kill -9 `sudo lsof -t -i:80` &
-#  sudo java -jar /home/ubuntu/actions-runner/_work/spring-aws/spring-aws/target/spring-aws-0.0.1-SNAPSHOT.jar
+java -jar spring-aws/target/spring-aws-0.0.1-SNAPSHOT.jar server config.json
